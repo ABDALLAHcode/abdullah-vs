b8684c667db20956057ce5f7ea38d50914b5ec28
ABDALLAHcode <abdullahbmyt@gmail.com>
2025-01-05 13:51:48 +0200

    change my pro

--- learn python.py
+++ learn python.py
@@ -1,3 +1,22 @@
-print('jjjjjjj')
-num=input('enter num')
-print (num)
+num1=float(input('enter first num:'))
+
+opy=input('enter opreation:')
+
+num2=float(input('enter second num:'))
+
+if opy == '+':
+    print(num1 + num2)
+elif opy == '-':
+    print(num1-num2)
+elif opy == '*':
+    print(num1*num2)
+elif opy == '**':
+    print(num1**num2)
+elif opy == '/':
+    print(num1/num2)
+elif opy == '//':
+    print(num1//num2)
+elif opy == '%':
+    print(num1%num2)
+else:
+    print('enter value correct')
